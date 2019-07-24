276e8ee2dd553aa7a8dc102e2a62f8c9a6488d25
Rivki Aizen <rivki@grth.co.il>
2019-07-24 19:55:53 +0300

    add server side data

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const logger = require('./logger');
 
+const fs = require("fs");
+const uuidv1 = require('uuid/v1');
+const bodyParser = require('body-parser');
+
 const argv = require('./argv');
 const port = require('./port');
 const setup = require('./middlewares/frontendMiddleware');
@@ -14,8 +18,48 @@ const ngrok =
 const { resolve } = require('path');
 const app = express();
 
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
+// rest contacts
+const jsonPath = `${__dirname}\\data\\contacts.json`;
+app.get('/api/contacts/list', (req, res) => {
+  fs.readFile(jsonPath, 'utf8', (err, data) => {
+    res.end(data);
+  });
+});
+
+app.post('/api/contacts/add', (req, res) => {
+  fs.readFile(jsonPath, 'utf8', (err, data) => {
+    data = JSON.parse(data);
+    const contact = req.body;
+    contact._id = uuidv1();
+    contact.index = data.length + 1;
+    const newData = [...data, contact];
+    const jsonData = JSON.stringify(newData);
+
+    fs.writeFile(jsonPath, jsonData, (writeFileErr) => {
+      if (!writeFileErr) {
+        res.end(jsonData);
+      } else {
+        res.end(JSON.stringify(data));
+      }
+    })
+  });
+});
+
+app.get('/api/contacts/:id', (req, res) => {
+  fs.readFile(jsonPath, 'utf8', (err, data) => {
+    const contacts = JSON.parse(data);
+    const contact = contacts.find(contactObj => contactObj._id === req.params.id) || {};
+    res.end(JSON.stringify(contact));
+  });
+});
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
@@ -34,6 +78,8 @@ app.get('*.js', (req, res, next) => {
   res.set('Content-Encoding', 'gzip');
   next();
 });
+
+
 
 // Start your app.
 app.listen(port, host, async err => {
