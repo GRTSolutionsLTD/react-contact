4e754d78d05237fcab1a49eede6a9d869ee2561d
Naomi Brand <naomi@grth.co.il>
2019-07-29 14:25:27 +0300

    fixed filter

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -3,20 +3,25 @@ import { Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import PropTypes from 'prop-types';
+
 import ContactList from 'containers/ContactList/Loadable';
+import HomePage from 'containers/HomePage/Loadable';
 import ViewContact from 'containers/ViewContact/Loadable';
 import ContactFriend from 'containers/ContactFriends/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import { getNews } from '../App/actions'
+import Header from '../../components/Header'
 
 import GlobalStyle from '../../global-styles';
 
-export function App({getContactList}) {
+export function App({ getContactList }) {
   getContactList();
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={ContactList} />
+        <Route exact path="/About" component={HomePage} />
         <Route path="/ContactList" component={ContactList} />
         <Route path="/ViewContact/:contactId" component={ViewContact} />
         <Route path="/ContactFriends/:contactId" component={ContactFriend} />
